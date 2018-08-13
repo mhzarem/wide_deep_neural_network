11a9f6bc8c4dcbfdeb8bb2117dd56d96f8a19d6c
Hossein Zare <mhzarem@gmail.com>
2018-08-13 16:01:28 +0430

    Update my_wide_deep.py

--- my_wide_deep.py
+++ my_wide_deep.py
@@ -11,6 +11,6 @@ deep_dense_3 = Dense(8, activation='relu', name='deep_dense_3')(deep_dense_2)
 
 wide_deep = concatenate([deep_dense_3, wide])
 
-ali = Dense(1, activation='sigmoid', name='wide_deep')(wide_deep)
+lay = Dense(1, activation='sigmoid', name='wide_deep')(wide_deep)
 
-end = Model(inputs=[deep, wide], outputs=ali)
+end = Model(inputs=[deep, wide], outputs=lay)
